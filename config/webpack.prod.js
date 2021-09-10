7f562b9f1bf860af44b78875ddf2d05efce8d048
wky <misterwky@gmail.com>
2021-09-10 17:57:05 +0800

    github pages

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -57,7 +57,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../src/template.html'),
-      filename: 'index.html'
+      filename: 'index.html',
+      publicPath: '/profile'
     }),
     new MiniCssExtractPlugin({
       filename: 'styles/styles.[hash].css'
