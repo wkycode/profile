295456c4e528f75e59c5127b578bcbfb9cd56c48
wky <misterwky@gmail.com>
2021-09-14 19:26:10 +0800

    action

--- src/components/ProjectsSection/ProjectsSectionElements.js
+++ src/components/ProjectsSection/ProjectsSectionElements.js
@@ -7,7 +7,7 @@ export const ProjectsContainer = styled.div`
   justify-content: center;
   align-items: center;
   background: #010606;
-  padding-bottom: 140px;
+  padding-bottom: 180px;
 
   @media screen and (max-width: 1050px) {
     height: 700px;
